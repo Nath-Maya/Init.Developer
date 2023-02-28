e6d70c2dad1fdf80e86c19d54b43b991c7f8e50c
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-02-27 23:25:50 -0500

    var, let, const

--- init-js/tipos-variables.js
+++ init-js/tipos-variables.js
@@ -14,6 +14,31 @@
 
    let soyDeveloper= true;
    console.log(soyDeveloper);
-   
+
+//! Variable CONST
+
+   //Espacio de memoria que no cambiara en el tiempo, es constante.
+
+   const valorPasaje= 1650;
+   console.log(valorPasaje);
+
+   const nombrePasajero= "Nathalia";
+   const apellidoPasajero= "Maya";
+
+//! Variable LET
+
+   //Espacio de memoria que puede cambiar en el tiempo, puede ser reemplazado.
+   //El alcance es dentro del bloque de codigo. 
+   // let nombrePasajero= "Nathy";
+   // console.log(nombrePasajero);
+
+   let nombreCompletoPasajero= nombrePasajero + " " + apellidoPasajero;
+   console.log(nombreCompletoPasajero);
+
+ //! Variable VAR
+    //Espacio de memoria que puede cambiar en el tiempo, puede ser reemplazado. El alcance es total en el programa.
+
+
+
 
 
