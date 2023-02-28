c53f9fdf06544ee05f2aef961e15b9d09bf3ab63
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-02-27 22:57:21 -0500

    Variables

---
+++ init-js/tipos-variables.js
@@ -0,0 +1,19 @@
+//! Tipos de Variables
+
+   //Texto o String
+
+   let developer= "Developer";
+   console.log(developer);
+
+   //Numerico
+
+   let cantDeveloper= 123;
+   console.log(cantDeveloper);
+
+   //booleano o logico 
+
+   let soyDeveloper= true;
+   console.log(soyDeveloper);
+   
+
+
