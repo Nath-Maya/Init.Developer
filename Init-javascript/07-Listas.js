db85ac66c2162a28701cbaa9a26814b32500e2a1
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-15 20:42:51 -0500

    otros

--- Init-javascript/07-Listas.js
+++ Init-javascript/07-Listas.js
@@ -1,25 +1,25 @@
 //* FUNCIONES Y PROPIEDADES DE UN ARRAY
 
-//!Definicion de una list de una forma abreviada
+//Definicion de una list de una forma abreviada
 
 const paisesDisponibles =["Colombia","Peru","Chile"];
 
 
-//!Definicion de una lista normal
+//Definicion de una lista normal
 
 const paisesDisponibles2 = new Array ("Colombia", "Peru","Argentina");
 
 
-//!Agregar un elemento al final de la lista
+//Agregar un elemento al final de la lista
 
 paisesDisponibles.push("Uruguay");
 console.log("Agregar elemento al final de la lista " + paisesDisponibles);
 
-//!Agregar un elemento al inicio de la lista 
+//Agregar un elemento al inicio de la lista 
 paisesDisponibles2.unshift("Ecuador");
 console.log("Agregar elemento al inicio de la lista con unshift " + paisesDisponibles2);
 
-//!Los array siempre inician en posicion 0.
+//Los array siempre inician en posicion 0.
 console.log(paisesDisponibles[0]);
 
 //! Otras funciones listas
@@ -32,11 +32,11 @@ const cantidadPaises = paisesDisponibles.length;
 const ciudadesColombia = ["Medellin","Cali","Bogota","Armenia","Barranquilla"];
 
 
-//!Eliminar elementos de la lista, el primer
+//Eliminar elementos de la lista, el primer
 ciudadesColombia.shift();
 
 
-//!Eliminar el ultimo elemento 
+//Eliminar el ultimo elemento 
 ciudadesColombia.pop();
 console.log(ciudadesColombia);
 
