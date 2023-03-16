c2ab4de7c98c652958ecdad4843b8a3fccff8001
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-15 19:06:39 -0500

    Conceptos de Js

---
+++ Init-javascript/07-Listas.js
@@ -0,0 +1,20 @@
+
+
+//!Definicion de una list de una forma abreviada
+
+const paisesDisponibles =["Colombia","Peru","Chile"];
+
+
+//! Definicion de una lista normal
+
+const paisesDisponibles2 = new Array ("Colombia", "Peru","Argentina");
+
+
+
+//?Agregar un elemento al final de la lista
+paisesDisponibles.push("Uruguay");
+console.log(paisesDisponibles);
+
+//*Agregar un elemento al inicio de la lista 
+paisesDisponibles2.unshift("Ecuador");
+console.log(paisesDisponibles2);
