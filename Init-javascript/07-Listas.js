5656021eb1cc051c9689a3e8f8bae2b7dde631b6
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-15 19:33:02 -0500

    Iniciar un array

--- Init-javascript/07-Listas.js
+++ Init-javascript/07-Listas.js
@@ -11,10 +11,13 @@ const paisesDisponibles2 = new Array ("Colombia", "Peru","Argentina");
 
 
 
-//?Agregar un elemento al final de la lista
+//!Agregar un elemento al final de la lista
 paisesDisponibles.push("Uruguay");
 console.log(paisesDisponibles);
 
-//*Agregar un elemento al inicio de la lista 
+//!Agregar un elemento al inicio de la lista 
 paisesDisponibles2.unshift("Ecuador");
 console.log(paisesDisponibles2);
+
+//Los array siempre inician en posicion 0.
+console.log(paisesDisponibles[0]);
