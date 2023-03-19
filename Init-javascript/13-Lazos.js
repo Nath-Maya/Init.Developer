1b7917e374a8d07eac24535a14e6cb2452fd4d92
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-19 11:55:36 -0500

    Bucle while

---
+++ Init-javascript/13-Lazos.js
@@ -0,0 +1,22 @@
+
+
+const ciudadesDisponibles = new Array("Bogota","Lima","Santiago","Montevideo");
+const precioCiudad = new Array(500,400,380,200);
+const presupuestoDisponible = 110;
+
+let i = 0;
+
+//!While...0 o mas veces.
+//*Do... 1 o mas veces
+
+while(precioCiudad[i] > presupuestoDisponible && ciudadesDisponibles.length) {
+  i++;
+  //Si se nos olvida colocar el contador queda en un bucle infinito.
+}
+
+if (i == ciudadesDisponibles.length)
+  console.log("No tenemos pasajes disponibles");
+else
+console.log("Puedes comprar pasaje para: " + ciudadesDisponibles[i]);
+
+
